97693d4e45a2a295603592aac3f2994db892da4b
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2021-11-09 15:27:36 +0100

    fake page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
 {
@@ -16,6 +17,21 @@ class HomeController extends AbstractController
         return $this->render("home.html.twig", [
             "name" => "Maxime",
             "age" => 25
+        ]);
+    }
+
+    /**
+     * @Route("/fake", name="fake")
+     */
+    public function fake(HttpClientInterface $httpClient): Response
+    {
+        $response = $httpClient->request(
+            'GET',
+            'https://jsonplaceholder.typicode.com/users'
+        );
+
+        return $this->render("fake.html.twig", [
+            "users" => $response->toArray()
         ]);
     }
 }
